d2be7a4a57962589153cdcba225eb0764990712d
DarioMarimon <dar.mar.gar2000@gmail.com>
2024-11-18 09:07:20 +0100

    feat(App.vue): script is now script setup
    Also, styles changed.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
   <MainHeader />
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    MainHeader: () => import("./components/MainHeader.vue"),
-  },
-};
+<script setup>
+import MainHeader from "./components/MainHeader.vue";
 </script>
 
 <style>
+@import url(assets/base.css);
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
